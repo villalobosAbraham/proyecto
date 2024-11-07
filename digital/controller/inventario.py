313685173bffcd82fdb3988d737f8c3a680dbc2c
villalobosAbraham <abrahamvillalobos3656@gmail.com>
2024-11-06 20:09:07 -0700

    algo es algo

--- digital/controller/inventario.py
+++ digital/controller/inventario.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def INVObtenerLibrosPopulares() :
+def INVObtenerLibrosPopulares(request) :
     resultado = inventario_model.INVObtenerLibrosPopulares()
 
     return JsonResponse(resultado, safe=False)
--- digital/modelos/inventario_model.py
+++ digital/modelos/inventario_model.py
@@ -1,21 +1,38 @@
 from django.db.models import Max, F
-from django.db import transaction, IntegrityError
-from digital.models import LogUsuarios, Visualizacion, CatLibros, InventarioLibro
+from django.db import transaction, IntegrityError, connection
+import psycopg2
+from psycopg2.extras import DictCursor
 
 def INVObtenerLibrosPopulares() :
-    # Filtrar las visualizaciones con las condiciones dadas y unir tablas necesarias
-    visualizaciones = (
-        Visualizacion.objects
-        .filter(
-            catlibros__activo='S',                          # cat_libros.activo = 'S'
-            invinventariolibros__cantidad__gt=0,            # inv_inventariolibros.cantidad > 0
-            invinventariolibros__cantidad__isnull=False     # inv_inventariolibros.cantidad IS NOT NULL
-        )
-        .select_related('catlibros')                        # Equivale al JOIN de cat_libros
-        .select_related('inv_inventariolibros')             # Equivale al LEFT JOIN de inv_inventariolibros
-        .values('idlibro')                                  # Agrupar por idlibro
-        .annotate(fecha_max=Max('fecha'))                   # Obtener MAX(fecha) por cada idlibro
-        .order_by('-fecha_max')[:10]                        # Ordenar y limitar a los 10 más recientes
-    )
+    idsLibrosPopulares = obtenerIdsLibrosPopulares()
 
-    return visualizaciones
+
+    return idsLibrosPopulares
+
+def obtenerIdsLibrosPopulares() :
+    sql = """SELECT id
+            FROM (
+                SELECT 
+                    inv_visualizaciones.id, inv_visualizaciones.fecha
+                FROM 
+                    inv_visualizaciones
+                JOIN
+                    cat_libros ON inv_visualizaciones.idlibro = cat_libros.id
+                LEFT JOIN
+                    inv_inventariolibros ON inv_visualizaciones.idlibro = inv_inventariolibros.idlibro
+                WHERE
+                    cat_libros.activo = 'S' AND
+                    inv_inventariolibros.cantidad > 0 AND
+                    inv_inventariolibros.cantidad IS NOT NULL
+                ORDER BY 
+                    fecha DESC
+            ) as subquery
+            GROUP BY id
+            ORDER BY MAX(fecha) DESC
+            LIMIT 10 """
+    
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        resultados = cursor.fetchall()
+
+    return [{"id" : resultado[0]} for resultado in resultados]
